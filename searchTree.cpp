846473b8b99e2e741b468e405c590dd09a175102
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-06-11 18:03:29 +0700

    menambahkan if ptr null

--- searchTree.cpp
+++ searchTree.cpp
@@ -78,3 +78,10 @@ public:
             cout << "Tree is empty" << endl;
             return;
         }
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
