9b724439950036226885ca44fa18f78431c867c1
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-06-11 17:56:18 +0700

    membuat class binarytree

--- searchTree.cpp
+++ searchTree.cpp
@@ -7,4 +7,21 @@ class node
     publik : string info;
     node *leftchild;
     node *rightchild;
+
+    node(string i, node *l, node *r)
+    {
+        info = i;
+        leftchild = l;
+        right = r;
+    }
+};
+
+class binarytree
+{
+public:
+    node *root;
+    binarytree()
+    {
+        root = null; // initializingroot to nul
+    }
 }
