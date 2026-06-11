bc866baea7a4bb52dfa610cd63e931c3ee8bfcd4
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-06-11 18:05:09 +0700

    menambahkan isi code dari void postorder

--- searchTree.cpp
+++ searchTree.cpp
@@ -86,3 +86,19 @@ public:
         }
     }
     void postorder(Node *ptr)
+    {
+        // perfoms the postorder traveersal of the tree
+
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
+};
